64c9e4c0309761aa9f1630313acea819a55e1967
= <seanalunni@gmail.com>
2021-01-14 13:23:19 +0100

    conda

--- conda.sh
+++ conda.sh
@@ -8,6 +8,6 @@ curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 # Installazione
 TMPDIR=$PWD bash Miniconda3-latest-Linux-x86_64.sh
 
-# Uscita cartella temporanea
+# Cancellazione cartella temporanea
 cd ..
 rm -r tempInstaller
